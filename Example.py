81264be15579a910427aa674b363ebc427522578
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-10-15 11:51:04 +0100

    Update and rename Week_5ex.py to Example.py

---
+++ Example.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter first number: ")) #first time i wrote this i forgot the last parenthesis
+num2 = int(input("Enter second number: ")) #same as above
+print("Quotient using // is", "\t", num1//num2)
+print("Quotient using / is", "\t", num1/num2)
+print("Quotient using % is", "\t", num1%num2)
--- Week_5ex.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Enter first number: ")
-num2 = int(input("Enter second number: ")
-print("Quotient using // is", "\t", num1//num2)
-print("Quotient using / is", "\t", num1/num2)
-print("Quotient using % is", "\t", num1%num2)
