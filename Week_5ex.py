3407595d5be8dc01aee4b740f3d2bfab5a903d14
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-10-15 11:30:38 +0100

    Create Week_5ex.py
    class exercise about math operations

---
+++ Week_5ex.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter first number: ")
+num1 = int(input("Enter second number: ")
+print("Quotient using // is", "\t", num1//num2)
+print("Quotient using / is", "\t", num1/num2)
+print("Quotient using % is", "\t", num1%num2)
